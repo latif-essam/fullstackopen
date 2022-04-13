8c3223d325ddfc02c24c837a107859e52586a1d9
Latif Essam <latifekhaliel@gmail.com>
2022-04-13 15:04:14 +0200

    part01-anecdotes finised exercises 1.13

--- part01/anecdotes/src/App.js
+++ part01/anecdotes/src/App.js
@@ -1,6 +1,5 @@
+
 import { useState } from 'react';
-
-
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -11,16 +10,36 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ];
+  const random = () => Math.floor(Math.random() * anecdotes.length);
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(Array.apply(null, new Array(anecdotes.length)).map(point => point = random()));
 
-  const [selected, setSelected] = useState(0)
+  const handleClick = () => {
+    setSelected(random);
+  }
+  const handleVote = () => {
+    const newPoints = [...points];
+    newPoints[selected] += 1;
+    setPoints(newPoints);
+  }
+  console.log({ selected, points })
   return (
     <div>
-      <nav>
+      <nav style={{ textAlign: 'center' }}>
         <h1>Ancedots of Software Engineering</h1>
       </nav>
       <main>
-        <p>"{anecdotes[selected]}".</p>
-        <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))} >Wisdom 🙏</button>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '90vw' }}>
+          <div style={{ boxShadow: '0px 0px 10px grey', borderRadius: 10, padding: 10 }}>
+            <p>"{anecdotes[selected]}",</p>
+            <p>has {points[selected]}</p>
+          </div>
+          <br />
+          <div>
+            <button onClick={handleVote} >Vote</button>
+            {" "}<button onClick={handleClick} >Wisdom 🙏</button>
+          </div>
+        </div>
       </main>
     </div>
   );
