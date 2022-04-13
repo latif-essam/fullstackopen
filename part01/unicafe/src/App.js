c573783df5ab83813ddf6eb38cfc3d8e78ffedf3
Latif Essam <latifekhaliel@gmail.com>
2022-04-13 13:40:12 +0200

    part01-unicafe finised exercises 1.10

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
 const Button = ({ handleCLick, text }) => (<button onClick={handleCLick}>{text}</button>)
+const StatisticLine = ({ text, value }) => <p>{text + " " + value}{text === 'postive' && ' %'}</p>
+
 const Statistics = ({ good, bad, neutral }) => {
   const totalFeedback = good + bad + neutral;
   const average = (good - bad) / totalFeedback;
@@ -10,12 +12,12 @@ const Statistics = ({ good, bad, neutral }) => {
     <>
       <h2>Statistics</h2>
       <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {totalFeedback}</p>
-        <p>average {average || 0}</p>
-        <p>postive {postive * 100 || 0} %</p>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={totalFeedback} />
+        <StatisticLine text="average" value={average || 0} />
+        <StatisticLine text="postive" value={postive * 100 || 0} />
       </div>
     </>)
 }
