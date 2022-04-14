beccfae6b2598c709dc4504105be2d65af685a04
Latif <latifekhaliel@gmail.com>
2022-04-14 21:06:29 +0200

    part02-courseinfo finished exercises 2.1 to 2.5

--- part01/courseinfo/src/components/Total.js
+++ part01/courseinfo/src/components/Total.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Total = ({ parts }) => {
     const total = parts.reduce((acc, obj) => acc += obj.exercises, 0);
     return (
-        <p>Number of exercises {total}</p>
+        <p><strong>
+            Total number of exercises: {total}
+        </strong> </p>
     );
 }
 
